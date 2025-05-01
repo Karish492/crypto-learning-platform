03bb2f7cea50378c21ac02859e6eb26b8bb88089
ry00283 <ry00283@surrey.ac.uk>
2025-05-01 11:54:36 +0100

    connected backend to frontend for module pages

--- frontend/src/pages/Module.js
+++ frontend/src/pages/Module.js
@@ -2,32 +2,45 @@ import React, { useState, useEffect } from 'react';
 import { useParams} from 'react-router-dom';
 import Error from '../components/Error';
 import Button from '../components/Button';
-
+import { useQuery } from '@tanstack/react-query';
 
 //mock data for module page to see how it looks etc.
-const modules = [
+/*const modules = [
   { id: 1, name: "Intro to Blockchain", description: "Learn about blockchain basics." },
   { id: 2, name: "Smart Contracts", description: "Understanding smart contracts." },
   { id: 3, name: "DeFi", description: "Explore Decentralized Finance." },
-];
+]; */
+
 const Module = () => {
-  const { id } = useParams();
+  const {id} = useParams();
   const [module, setModule] = useState(null);
   // Pass data from Modules to Module using props instead of mock data this is just for testing and front end.
-  useEffect(() => {
-    const selected = modules.find(m => m.id === parseInt(id));
-    setModule(selected);
-  }, [id]);
+  const {isLoading, error, data} = useQuery({
+        queryKey: ['modules'],
+        queryFn: async () => {
+            const response = await fetch(`http://localhost:8000/api/module/${id}`);
+            if (!response.ok) throw new Error('Network response was not ok');
+            return response.json();
+        },
+    
+        });
+  
+        if (isLoading) {
+          return <h1> Loading...</h1>
+        }
+      
+        if (error) {
+          return 'An Error Has Occured: ' + error.message
+        }
 
-  if (!module) return <Error message = "Error : Module details not found, Please try again later. "/>;
 
  
   return (
     <div className="flex flex-row min-h-screen p-6 gap-6">
       <div className="flex-1 bg-white rounded-lg shadow-lg p-6">
-        <h1 className="text-4xl font-bold mb-4">{module.name}</h1>
-        <p className="text-lg mb-6">Welcome to this module on Cryptocurrency Fundamentals. In this section, you'll gain a clear understanding of what cryptocurrencies are, how blockchain technology underpins them, and why they have gained so much attention in recent years. Through interactive lessons, real-world examples, and visual aids, we’ll explore key concepts such as decentralization, digital wallets, mining, and the role of cryptographic security. By the end of this module, you’ll be equipped with the foundational knowledge needed to confidently navigate the world of crypto and prepare for more advanced topics in future modules.</p>
-        <Button to = {`/quiz/${module.id}`} text = "Start Quiz" />
+        <h1 className="text-4xl font-bold mb-4">{data.title}</h1>
+        <p className="text-lg mb-6">{data.description}</p>
+        <Button to = {`/quiz/${data.module_id}`} text = "Start Quiz" />
         
       </div>
 
@@ -40,7 +53,7 @@ const Module = () => {
         />
         <img
           src="/bitcoin_cover.jpg"
-          alt={module.name}
+          alt={data.title}
           className="w-full h-64 object-cover rounded-md shadow"
         />
       </div>
--- frontend/src/pages/Modules.js
+++ frontend/src/pages/Modules.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ModuleCards from '../components/ModuleCard';
-
+import { useQuery } from '@tanstack/react-query';
 const Modules = () => {
-  //HERE PLACE QUERY FOR BACKEND API WHERE YOU SAVE SAID MODULES AND THE INFORMATION INSTEAD OF THE CONSTANT MODULES.
-  // This is just mock data to make sure front end is okay.
-  const modules = [
-    { id: 1, name: "Introduction to Blockchain", image: "bitcoin_cover.jpg" },
-    { id: 2, name: "Crypto Trading Strategies", image: "bitcoin_cover.jpg" },
-    { id: 3, name: "DeFi (Decentralized Finance)", image: "bitcoin_cover.jpg" },
-    { id: 4, name: "Crypto Security", image: "bitcoin_cover.jpg" },
-  ];
+    const {isLoading, error, data} = useQuery({
+      queryKey: ['modules'],
+      queryFn: async () => {
+          const response = await fetch("http://localhost:8000/api/module/");
+          if (!response.ok) throw new Error('Network response was not ok');
+
+          return response.json();
+      },
+  
+      });
+
+      if (isLoading) {
+        return <h1> Loading...</h1>
+      }
+    
+      if (error) {
+        return 'An Error Has Occured: ' + error.message
+      }
+      if (!Array.isArray(data)) {
+        return 'An Error Has Occured: ';
+      } //otherwise crashes when going from specific module to all modules
 
   return (
     <div className="container mx-auto p-4">
       <h2 className="text-4xl font-bold mb-8 text-center"> Modules </h2>
       <div className="grid grid-cols-3 gap-4">
-        {modules.map((module) => (
-          <Link to={`/modules/${module.id}`} style={{ textDecoration: "none" }}>
+        
+        {data.map((data) => (
+          <Link to={`/modules/${data.module_id}`} style={{ textDecoration: "none" }}>
             <ModuleCards
-              id = {module.id}
-              name={module.name}
-              image={module.image}
+              id = {data.module_id}
+              name={data.title}
+              image={"bitcoin_cover.jpg"}
             />
           </Link>
         ))}
